a505d79e13db4e53118ab53e97eedd52771a88c1
Aswin Karuvally <aswinbabuk@gmail.com>
2018-03-13 23:25:34 +0530

    updated upload script

--- thingspeak/upload.py
+++ thingspeak/upload.py
@@ -11,7 +11,7 @@ import random
 def upload_data():
     # generate a upload data
     upload_value = str(random.randint(1, 100))
-    print('uploading the value' + upload_value)
+    print('uploading the value ' + upload_value)
     requests_object = requests.get('https://api.thingspeak.com/update?api_key=AF3WSB8SADM89PS1&field1=' + upload_value)
 
 upload_data()
