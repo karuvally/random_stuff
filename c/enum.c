6feb78c78c5081cb414ef557778f9a101b6de9e7
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2018-12-27 22:12:39 +0530

    added some simple c stuff

---
+++ c/enum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{
+	enum my_first_enum{value0, value1, value2, value3};
+
+	printf("%d\n", value1);
+	return 0;
+}
---
+++ c/not.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+	int i = 5;
+	int j = !i;
+
+	printf("%d\n", j);
+	return 0;
+}
