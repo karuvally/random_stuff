f00c718262df71fc51b6d947564718306f82bcbb
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2019-09-10 21:43:22 +0530

    added test file

---
+++ libcurl/test.c
@@ -0,0 +1,12 @@
+// import the serious stuff
+#include<stdio.h>
+#include<curl/curl.h>
+
+
+// the main function
+int main()
+{
+	// global init stuff
+	curl_global_init(CURL_GLOBAL_ALL);
+	return 0;
+}
