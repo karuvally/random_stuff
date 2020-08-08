b4accc56f782707393fb039b66b473cba95b4901
Aswin Karuvally <aswinbabuk@gmail.com>
2020-08-08 12:27:52 +0530

    added easter eggs view

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello World!, You are at the polls :)")
 
+
+def easter_eggs(request):
+    return(HttpResponse("There are no easter eggs here.")
+
