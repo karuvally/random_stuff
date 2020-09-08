9eb5af7dd77bb68dff931ca28ba95701fdd22651
Aswin Karuvally <aswinbabuk@gmail.com>
2020-09-08 20:16:17 +0530

    added dog class

--- python_super/super_test.py
+++ python_super/super_test.py
@@ -8,3 +8,12 @@ class Animal:
 
     def speak():
         print("Animals produce wide variety of sounds...")
+
+class Dog(Animal):
+    def __init__(self):
+        no_of_legs = 4
+        can_run = True
+        can_jump = True
+
+    def speak():
+        print("Bow Bow, I am the dog of wisdom!")
