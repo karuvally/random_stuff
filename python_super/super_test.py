a37a4ca7690846249150db6d6586b4af6938ed0b
Aswin Karuvally <aswinbabuk@gmail.com>
2020-09-08 20:29:05 +0530

    added main() function

--- python_super/super_test.py
+++ python_super/super_test.py
@@ -4,25 +4,32 @@
 
 class Animal:
     def __init__(self):
-        is_plant = False
+        self.is_plant = False
 
     def speak():
         print("Animals produce wide variety of sounds...")
 
 class Dog(Animal):
     def __init__(self):
-        no_of_legs = 4
-        can_run = True
-        can_jump = True
+       self.no_of_legs = 4
+       self.can_run = True
+       self.can_jump = True
 
     def speak():
         print("Bow Bow, I am the dog of wisdom!")
 
 class GoldenRetriever(Dog):
-    def __init(self):
-        color = "Gold (duh!)"
-        is_cute = True
+    def __init(self, name):
+        self.color = "Gold (duh!)"
+        self.is_cute = True
+        self.name = name
 
     def fetch():
         print("Here is the stick you threw, -")
+
+
+# The function where everything happens
+def main():
+    # Lets create a GoldenRetriever object
+    sparky = GoldenRetriever("Sparky")
 
